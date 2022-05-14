be6a056d97c9f2251d052c3610e18eab4bf60b47
adrianncovaci <adrianmihaicovaci@gmail.com>
2022-05-14 21:47:23 +0300

    Initial Commit

---
+++ src/instruction.rs
@@ -0,0 +1,6 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(BorshSerialize, BorshDeserialize, Clone, Debug)]
+pub enum CounterInstruction {
+    Increment,
+}
---
+++ src/processor.rs
@@ -0,0 +1,35 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+use crate::{instruction::CounterInstruction, state::Counter};
+
+pub struct Processor {}
+
+impl Processor {
+    pub fn process_instruction(
+        _program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        let instruction = CounterInstruction::try_from_slice(&instruction_data)
+            .map_err(|_| ProgramError::InvalidInstructionData)?;
+        match instruction {
+            CounterInstruction::Increment => {
+                msg!("Incrementind counter");
+                let mut accounts_iter = accounts.iter();
+                let count_acc = next_account_info(&mut accounts_iter)?;
+                let mut counter = Counter::try_from_slice(&count_acc.data.borrow())?;
+                counter.value += 1;
+                msg!("Updating count {}", counter.value);
+                counter.serialize(&mut *count_acc.data.borrow_mut())?;
+            }
+        }
+        Ok(())
+    }
+}
